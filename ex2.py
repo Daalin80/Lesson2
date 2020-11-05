986b6858f261cb93a4a8049dfd212fe5f7a20e24
Daalin80 <73665109+Daalin80@users.noreply.github.com>
2020-11-05 16:40:47 +0200

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,5 @@
+number = int(input("Please enter integer number: "))
+next = number + 1
+prev = number - 1
+print(f"The next number for the number {number} is {next}.")
+print(f"The previous number for the number {number} is {prev}")
